44ed1916c7dd24ca36318dc80056476b6a91e9bd
davidtr1820 <davidhack18@gmail.com>
2017-09-08 23:47:16 -0500

    algun comentario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    comiensa el curso
                 </div>
 
                 <div class="links">
